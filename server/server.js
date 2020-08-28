df59a74e4520defd5d6c69a1c9b49d3339cea78d
Combatd <combatdstar@gmail.com>
2020-08-28 10:42:45 -0700

    Res send JSON on root

--- server/server.js
+++ server/server.js
@@ -4,8 +4,27 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use('/', () => {
-    console.log('root route was hit!');
+app.use('/', (req, res) => {
+    res.send(
+        [
+            {
+                name: 'Markie',
+                animal: 'dog',
+                age: 14
+            },
+            {
+                name: 'Dandyio',
+                animal: 'bird',
+                age: 17
+            },
+            {
+                name: 'Manio',
+                animal: 'cat',
+                age: 45
+            }
+        ]
+    );
+
 });
 
 app.listen(PORT, console.log(`${PORT} running server.js`));
